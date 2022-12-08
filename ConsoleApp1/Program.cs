451a77da078b6fbfa855f4ec0a38c5cabeadede8
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-08 23:28:09 +0900

    p105_1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ class P105_1
         double s, l; //삼각형의 넓이s와 세 변의 길이의 합 나누기 2인 l
         Console.Write("삼각형의 세 변의 길이를 입력하시오.  : ");
         a = Console.Read();
-        b = Console.Read();
+        b = Console.Read();  //a,b,c 문자코드값 리턴
         c = Console.Read(); //입력받은 세 변의 문자코드값을 리턴
         l = (a + b + c) / 2; //세 변의 합을 2로 나눈 값
         s = Math.Sqrt(l* (l - a) * (l - b) * (l - c)); //헤론의 공식을 사용하여 삼각형의 넓이를 구함
